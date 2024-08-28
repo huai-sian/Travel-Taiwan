aa4b98891416c9cc1316f9c41065493b172bd4c8
huai-sian <127100366+AnaHuang1114@users.noreply.github.com>
2024-08-28 14:25:25 +0800

    feat: add sentry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 import "./assets/scss/all.scss";
+import * as Sentry from "@sentry/vue";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: "https://9fdfc59ec7f15f79862cd379ee2f5363@o4507849872113664.ingest.us.sentry.io/4507849873358848",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    "localhost",
+    /^https:\/\/huai-sian\.github\.io\/Travel-Taiwan/,
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+app.use(router).mount("#app");
